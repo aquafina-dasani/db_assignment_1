d606db89729e97aea39a6d752e0d5862370fb297
Nick <nickcao24@gmail.com>
2025-09-27 18:12:27 -0700

    Finished SQL Injection insecure file

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def login_db(credentials):
 
     try:
         my_db = mysql.connector.connect(
-            hostname = credentials["hostname"],
+            host = credentials["hostname"],
             port = credentials["port"],
             user = credentials["username"],
             passwd = credentials["password"],
@@ -40,30 +40,60 @@ def login_db(credentials):
         return my_db
 
 
-    except:
+    except Exception as e:
+        print(e)
         sys.exit("Could not login to host with user/password provided.")
 
 
-if __name__ == "__main__":
+def show_version(my_cursor):
+    """Simply show the version of MySQL after logging in"""
+    my_cursor.execute("SHOW VARIABLES LIKE 'version'")
+    print(my_cursor.fetchone())
+
+
+def prompt_word(my_cursor, my_db):
+    """Add word to database if it doesn't exist. Change word in database if it does exist."""
+    first_word = input("What word would you like to add/change >> ")
+
+    # See if word is in database already
+    my_cursor.execute(f"SELECT word FROM dictionary.word WHERE word = '{first_word}'")
+    check = my_cursor.fetchone()
+
+
+    if check:
+        print(f"Found '{first_word}' in the database.")
+        print(check)
+
+        second_word = input(f"Change {first_word} to >> ")
+        my_cursor.execute(f"UPDATE dictionary.word SET word = '{second_word}' WHERE word = '{first_word}'")
+
+        print(f"Changed '{first_word}' to '{second_word}' in the database")
+    
+
+    else:
+        print(f"The word '{first_word}' was not found... adding")
+        my_cursor.execute(f"INSERT INTO dictionary.word (word) VALUES ('{first_word}')")
+
+    my_db.commit()
+
+def main():
+    # Get the user login details
     credentials = launch()
 
-
+    # boring init
     my_db = login_db(credentials)
+    my_cursor = my_db.cursor()
+
+    # display mysql version
+    show_version(my_cursor)
+
+    # main word prompt stuff
+    prompt_word(my_cursor, my_db)
+
+    # exit when finished
+    sys.exit("Goodbye")
 
 
-    while my_db:
-        my_cursor = my_db.cursor()
 
-
-        my_cursor.execute("SHOW VARIABLES like 'version'")
-
-
-        print(my_db)
-
-
-        my_result = my_cursor.fetchall()
-
-
-        for row in my_result:
-            print(row)
-
+if __name__ == "__main__":
+    main()
